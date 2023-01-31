86eac815d9c48242343e3b75df464ae15acbcf01
fixda <Seanmurphy26@gmail.com>
2023-01-30 21:12:56 -0600

    Updates

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ export default function App() {
   const [coverPage, setCoverPage] = React.useState(true)
 
   function handleStart(){
-    console.log("HandleStart Clicked")
     return(
       setCoverPage(!coverPage)
     )
--- src/Quiz.js
+++ src/Quiz.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
 export default function Quiz() {
-  return (
-    <div>
-        <h2>Test</h2>
-    </div>
-  )
+
+    React.useEffect(()=>{
+        fetch("https://opentdb.com/api.php?amount=4&difficulty=medium&type=multiple")
+            .then(res=> res.json())
+            .then(data => console.log(data))
+    },[])
+
+    return (
+        <div className="quiz-container">
+            <h2>Test</h2>
+        </div>
+     )
 }
