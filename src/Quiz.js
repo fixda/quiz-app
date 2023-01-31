17635dfa9a7c2bf32b23703c95755b259a245691
fixda <Seanmurphy26@gmail.com>
2023-01-31 12:56:38 -0600

    Added Pictures

--- src/Quiz.js
+++ src/Quiz.js
@@ -2,12 +2,24 @@ import React from 'react'
 
 export default function Quiz() {
 
-    React.useEffect(()=>{
-        fetch("https://opentdb.com/api.php?amount=4&difficulty=medium&type=multiple")
-            .then(res=> res.json())
-            .then(data => console.log(data))
-    },[])
+    const [questions, setQuestions] = React.useState([])
 
+    React.useEffect(()=>{
+        fetch("https://opentdb.com/api.php?amount=5&difficulty=easy")
+            .then(res=> res.json())
+            .then(data => setQuestions(data.results))
+            
+            
+    },[])
+  
+    console.log(questions[0])
+    // function displayQuestions(){
+    //     return(
+    //         <div>
+    //             <h3>{questions[0]}</h3>
+    //         </div>
+    //     )
+    // }
     return (
         <div className="quiz-container">
             <h2>Test</h2>
